cd90feccdf9d437314eecf9288be2033bfa2ceda
Akihiko Nambu <mate0rich.nmb@gmail.com>
2015-02-19 12:27:25 +0900

    fix

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GnSns::Application.config.secret_key_base = '307e55c13c17d2d96cf03e9430b4364a7f8699d3b9be182dfb43409858e09e689ebb70824d6013d8adfeeca31c3549780929816b10e66986a1a020b6a127e49c'
