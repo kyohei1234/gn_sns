262e5c98a652d950242facaf346bc2db93d8f96e
Akihiko Nambu <mate0rich.nmb@gmail.com>
2015-02-21 00:47:10 +0900

    partial

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150219173721) do
+ActiveRecord::Schema.define(version: 20150219173856) do
 
   create_table "blogs", force: true do |t|
     t.string   "title"
