27eecec9c3b7548c1a109634ed94b735e73883c6
Akihiko Nambu <mate0rich.nmb@gmail.com>
2015-05-14 13:49:55 +0900

    fix

--- app/helpers/boards_helper.rb
+++ app/helpers/boards_helper.rb
@@ -1,5 +1,5 @@
 module BoardsHelper
-  def board_image_for(board)
-    image_tag "/docs/boards/#{board.image}"
+  def board_image_for(board, size="300")
+    image_tag "/docs/boards/#{board.image}", size: size
   end
 end
