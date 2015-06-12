c02a283a32acff6889c32fda283ee5e6f40f1bce
Akihiko Nambu <mate0rich.nmb@gmail.com>
2015-06-12 23:07:12 +0900

    stylesheetとjqueryの読み込みを修正

--- app/assets/javascripts/about.js
+++ app/assets/javascripts/about.js
@@ -1,17 +1,17 @@
-//= require jquery
-//= require jquery_ujs
+// require jquery
+// require jquery_ujs
 
 
-//= require application/application.js
-//= require bootstrap/bootstrap.js
-//= require plugins/jquery.bxslider
-//= require plugins/jquery.centralized.js
-//= require plugins/jquery.fixedonlater.js
-//= require plugins/jquery.hashloader.js
-//= require plugins/jquery.mixitup.js
-//= require plugins/jquery.nav.js
-//= require plugins/jquery.parallax-1.1.3.js
-//= require plugins/jquery.responsivevideos.js
-//= require plugins/jquery.scrollTo-1.4.3.1.js
-//= require plugins/jquery.tweet.js
-//= require plugins/jquery.tweetCarousel.js
+// require application/application.js
+// require bootstrap/bootstrap.js
+// require plugins/jquery.bxslider
+// require plugins/jquery.centralized.js
+// require plugins/jquery.fixedonlater.js
+// require plugins/jquery.hashloader.js
+// require plugins/jquery.mixitup.js
+// require plugins/jquery.nav.js
+// require plugins/jquery.parallax-1.1.3.js
+// require plugins/jquery.responsivevideos.js
+// require plugins/jquery.scrollTo-1.4.3.1.js
+// require plugins/jquery.tweet.js
+// require plugins/jquery.tweetCarousel.js
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root  'about#index'
+  root 'about#index'
   resources :boards
   get "about/index"
   resources :blogs
